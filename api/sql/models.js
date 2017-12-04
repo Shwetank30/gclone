a4b5616275ed2e8a096457cfefa5236f9973cba9
Shwetank30 <shwetank.kumar.cer13@itbhu.ac.in>
2017-12-04 19:44:27 +0530

    Queries refactored

--- api/sql/models.js
+++ api/sql/models.js
@@ -1,33 +1,51 @@
 import knex from './connector';
 
-export class Entries {
-  getForFeed(type, after) {
-    if(type !== 'NEW') {
-      throw new Error('Only NEW feed implemented so far.');
+function addSelectToEntryQuery(query) {
+  query.select('entries.*', knex.raw('SUM(votes.vote_value) as score'))
+    .leftJoin('votes', 'entries.id', 'votes.entry_id')
+    .groupBy('entries.id');
+}
+
+function convertNullColsToZero(row) {
+  row.score = row.score || 0;
+  return row;
+}
+
+function mapNullColsToZero(query) {
+  return query.then((rows) => {
+    if(rows.length) {
+      return rows.map(convertNullColsToZero);
     }
 
-    return knex('entries')
-      .select('entries.*',knex.raw('SUM(votes.vote_value) as score'))
-      .leftJoin('votes', 'entries.id', 'votes.entry_id')
-      .groupBy('entries.id')
-      .orderBy('created_at', 'desc').then((rows) => {
-        return rows.map((row) => {
-          row.score = row.score || 0;
-          return row;
-        });
-      });
+    return convertNullColsToZero(rows);
+  });
+}
+
+export class Entries {
+  getForFeed(type, after) {
+    const query = knex('entries')
+      .modify(addSelectToEntryQuery);
+
+    if (type === 'NEW') {
+      query.orderBy('created_at', 'desc');
+    } else if (type === 'TOP') {
+      query.orderBy('score', 'desc');
+    } else {
+      throw new Error(`Feed type ${type} not implemented.`);
+    }
+
+    return convertNullColsToZero(query);
   }
 
   getByRepoFullName(name) {
     //No need to batch
-    return knex('entries')
-      .select('*')
+    const query = knex('entries')
+      .modify(addSelectToEntryQuery)
       .where({ repository_name: name })
       .first();
-  }
 
-  // XXX also needs to return vote count
-
+      return convertNullColsToZero(query);
+    }
 }
 
 export class Comments {
